f8ad0097b17693b1f3f6bd33de6b89bb263c2558
Márcio Silva <marcio.silva@institutolibertas.org.br>
2026-09-24 16:07:15 -0300

    base do projeto gerada no initilizer

---
+++ src/main/java/br/com/lifehub/LifehubApplication.java
@@ -0,0 +1,13 @@
+package br.com.lifehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lifehub/LifehubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lifehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
